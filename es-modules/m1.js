6bbb4e3a186d3ec84d8b7e5231bd45672a811954
DivineCross <DivineCross@users.noreply.github.com>
2020-03-03 18:34:48 +0800

    Add practice 'es-modules'

---
+++ es-modules/m1.js
@@ -0,0 +1,11 @@
+// CommonJS: wrapping iife
+// (function(exports, require, module, __filename, __dirname) {
+    // client code
+    // exports === module.exports
+// })();
+
+// ES modules
+export default {
+    name: 'm1.js',
+    value: 1,
+};
---
+++ es-modules/m2.js
@@ -0,0 +1,7 @@
+import m1 from './m1.js';
+
+export default {
+    name: 'm2.js',
+    value: 2,
+    dependency: m1,
+};
